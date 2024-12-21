b3a67cc0739c1efc40459df496fb8b2f14118dd8
nilajhub <nilaj.chakraborty007@gmail.com>
2024-12-21 15:49:40 +0530

    button extranal module css

---
+++ React Projects/stylingcss/src/App.jsx
@@ -0,0 +1,33 @@
+
+
+ 
+
+/* Extranal CSS
+     .button{
+     
+     
+     background-color: hsl(200, 100%, 50%);
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+}
+      
+      
+      
+      */
+
+import Button from "./component/Button.jsx"
+
+function App() {
+
+  return(
+    <>
+     <Button/>
+    </>
+   
+  );
+}
+
+export default App
---
+++ React Projects/stylingcss/src/component/Button.jsx
@@ -0,0 +1,15 @@
+import styles from './button.module.css'
+
+function Button(){
+
+
+    return(
+
+<button className={styles.button}>Click me </button>
+
+
+    );
+}
+
+export default Button
+ 
