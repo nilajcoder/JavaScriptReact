c56cf319a4d0979990006b082a1c7104f488c30c
nilajhub <nilaj.chakraborty007@gmail.com>
2024-12-31 23:14:15 +0530

    most reapeated functions questions

---
+++ JavaScript Interview/demo.js
@@ -0,0 +1,71 @@
+
+/*
+Empty the array  splice method
+
+*/
+const arr=[1,2,3]
+arr.splice(0,arr.length)
+console.log(arr)//[]
+
+
+
+/*
+Map Method- return the new array with modication
+*/
+const a=[1,2,3]
+const b=a.map((n)=>n*2)
+console.log(b)//[2,4,6]
+
+
+/*
+Reduce method- return single value
+*/
+const x=[1,2,3]
+const y=x.reduce((acc,num)=>acc*num)
+console.log(y)//6
+
+
+/*
+Filter Method-Return the new array to filter
+*/
+
+const p=[1,2,3]
+const q=p.filter(item=>item>1)
+console.log(q)//[2,3]
+
+
+/*
+Replace method New Line
+*/
+const abc="Hello World"
+const xyz=abc.replace(" ","\n")
+console.log(xyz)
+
+/*
+Output
+
+Hello
+World
+*/
+
+/*
+
+Find method to find a specific value
+*/
+const v=[1,2,3]
+const vb=v.find(num=>num>2)
+console.log(vb)//3
+
+
+/*
+
+*/
+function infiniteocc(val1){
+    return function(val2){
+        if(!val2){
+            return val1
+        }
+        return infiniteocc(val1+val2)
+    }
+}
+console.log(infiniteocc(1)(2)())
