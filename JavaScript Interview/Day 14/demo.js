7d053a2581b5c0abe6d7bd47d1c9984a17950252
nilajhub <nilaj.chakraborty007@gmail.com>
2025-01-10 18:46:43 +0530

    day 14 interview js

---
+++ JavaScript Interview/Day 14/demo.js
@@ -0,0 +1,24 @@
+
+/*
+1. function checkAge(data) {
+    if (data === { age: 18 }) {
+        console.log("You are an adult");
+    } else if (data == { age: 18 }) {
+        console.log("You are still an adult");
+    } else {
+        console.log("Hmm....No age");
+    }
+}
+
+checkAge({ age: 18 }); Hmm....No age because it is an object refering diffrent memory location
+
+
+
+2. let a = { name: 'Adarsh' };
+let z = a;
+z.name = 'Adil';
+console.log(a.name); //Adil
+*/
+
+
+
