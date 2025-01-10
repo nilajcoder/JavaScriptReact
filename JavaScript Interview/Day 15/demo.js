c43028a348b321408256df20b570a565c7d54419
nilajhub <nilaj.chakraborty007@gmail.com>
2025-01-11 00:16:16 +0530

    Day 15 interview js

---
+++ JavaScript Interview/Day 15/demo.js
@@ -0,0 +1,51 @@
+ 
+ /*
+ 1. function fn(){
+         console.log("hai")
+ }
+ 
+ console.log(fn.name); //fn
+
+
+
+ 2. function abc(xyz){
+        console.log(~xyz);
+    }
+    
+    abc(1.12);//-2
+ 
+
+  3.  function abc(a, b, c = 0) {
+}
+
+console.log(abc.length);//2
+
+
+function abc(a, b, c ) {
+}
+
+console.log(abc.length);//3
+
+
+
+function abc(a, b=0, c ) {
+}
+
+console.log(abc.length);//1
+
+
+function abc(a=0, b, c ) {
+}
+
+console.log(abc.length);//0
+
+    */
+
+
+
+
+
+
+    
+
+    
