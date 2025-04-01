854a8fe443b700723ef8bec0535cdc6136025cf9
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-04-02 02:32:32 +0530

    mywebsite

---
+++ Html/WebSite/script/demo.js
@@ -0,0 +1,12 @@
+
+
+function redirectToLogin(event) {
+    event.preventDefault(); // Prevents default form submission behavior
+    window.location.href = "login.html"; // Redirects to login page
+}
+
+
+function redirectToRegister(event) {
+    event.preventDefault(); // Prevents default form submission behavior
+    window.location.href = "register.html"; // Redirects to register page
+}
