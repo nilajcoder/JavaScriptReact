2a85a51af0fb4f408dd20a243803cf84afcb40f9
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-06-05 20:41:39 +0530

    typeof

---
+++ Variables/typeof.js
@@ -0,0 +1,25 @@
+
+
+let x
+console.log(typeof x)
+
+let y=10
+console.log(typeof y)
+
+let z=10.25
+console.log(typeof z)
+
+let a=true
+console.log(typeof a)
+
+let b="nilaj"
+console.log(typeof b)
+
+let c=null
+console.log(typeof c)
+
+let d=BigInt(8)
+console.log(typeof d)
+
+let e=Symbol("hi")
+console.log(typeof e)
