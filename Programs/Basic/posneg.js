a0cf58014ad70a0c5b7a9a610241530b374ace5d
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-07-14 00:30:12 +0530

    Positive negetive Checking

---
+++ Programs/Basic/posneg.js
@@ -0,0 +1,21 @@
+
+/*
+POsitive Negative Check Number
+
+*/
+
+
+let a=10
+if (a>0) {
+    console.log("The Number is Positive ")
+
+    
+} else if(a<0){
+
+        console.log("The Number is Negative ")
+
+
+} else{
+            console.log("The Number is Zero ")
+
+}
