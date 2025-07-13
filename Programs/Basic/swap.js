0aa4d658f9326ecef2f86e00f4bba3d76739569e
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-07-14 00:19:08 +0530

    Swap Two Numbers

---
+++ Programs/Basic/swap.js
@@ -0,0 +1,26 @@
+
+
+
+/*
+
+WAP Swap Two Numbers
+*/
+
+let a=10;
+let b=20;
+console.log("Before Swap = ",a,b)
+ a=a+b;
+ b=a-b
+ a=a-b
+
+ console.log("After Swap = ",a,b)
+
+
+ let c=50
+ let d=60
+ console.log("Before Swap  =",c,d)
+ let t=c //50
+ c=d //60
+ d=t;//50
+  console.log("After Swap = ",c,d)
+ 
