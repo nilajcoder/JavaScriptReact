49c4b408329452e7060ff0196283b2c53c5bf11b
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-07-14 17:11:46 +0530

    prime

---
+++ Programs/Basic/prime.js
@@ -0,0 +1,22 @@
+
+
+
+/*
+
+WAP in
+*/
+let n=6
+let c=0
+for(let i=0;i<=n;i++){
+
+  if(n%i==0){
+    c++
+  }
+}
+
+if(c==2){
+    console.log("The Number is Prime ",n)
+} else{
+     console.log("The Number is not  Prime ",n)
+
+}
