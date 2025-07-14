adb18e8ba543f975ff11c8d5fb20c7f7c4e35879
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-07-14 12:16:15 +0530

    Array mcq

--- Array/arraymcq.js
+++ Array/arraymcq.js
@@ -1,3 +1,14 @@
+let arr = [,,,] 
+let arr2 = [1, 2, 3, 4] // 4
+let arr3 = [[1, 2, 3], [4, 5, 6]] // 2
+console.log(arr)// 3 empty items
+console.log(arr.length)//3
+console.log(arr2)
+console.log(arr2.length)//4
+console.log(arr3)
+console.log(arr3.length)//2 sub aaray
+
+
 
 
 console.log([] === [])      // false
