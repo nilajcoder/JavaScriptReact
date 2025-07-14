520f848d848dcd6844fc1e64d9d0a32cd4288e3e
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-07-14 12:01:17 +0530

    Empty array

---
+++ Programs/Array/emptyarr.js
@@ -0,0 +1,29 @@
+
+/*
+let arr=[1,2,3]
+
+arr.length=0
+console.log(arr)
+
+
+
+let arr=[1,2,3]
+arr=[]
+console.log(arr)
+
+
+let arr = [1, 2, 3];
+while (arr.length > 0) {
+  arr.pop();
+}
+console.log(arr); // []
+
+*/
+
+/*
+Empty The Array
+*/
+
+let arr=[1,2,3]
+arr.splice(0,arr.length)
+console.log(arr)
