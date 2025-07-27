d096b733aed974c50e78de2104d30b4afc0a3c90
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-07-27 21:29:26 +0530

    equality imp

---
+++ JavaScript Interview/Most Imp MCQ/equality.js
@@ -0,0 +1,82 @@
+
+
+let a=NaN
+let b=NaN
+console.log(a==b)// false
+console.log(a===b)//false
+
+console.log(false === 0);//false
+console.log(true === 0);//false
+console.log(true == 0);//false
+console.log(false == 0);//true
+console.log(0 == false);//true
+console.log(0 === false);//false
+console.log(typeof 0) //number
+console.log(typeof false)//boolean
+
+console.log(0 == true);//false
+console.log(0 === true);//false
+
+
+console.log(1 == true);//true
+console.log(1 === true);//false
+
+console.log(1 == false);//false
+console.log(1 === false);//false
+
+console.log(2 == true);//false
+console.log(3 === true);//false
+
+console.log(1 == "1") // true
+console.log(1 === "1") // false
+
+console.log(1 == 1) // true
+console.log(1 === 1) // true
+
+console.log(typeof NaN ==="number")//true
+
+console.log(typeof NaN =="number")//true
+
+
+console.log(true==true)//true
+
+console.log(true===true)//true
+
+
+
+console.log(true==false)//false
+
+console.log(true===false)//false
+
+
+
+console.log(null == undefined);//true
+console.log(null === undefined);//false
+
+console.log([] == false);//true
+console.log([] === false); //false
+
+console.log( [] ==true)//false
+
+console.log( [] === true)//false
+
+
+
+
+console.log("" == 0); //true
+
+console.log("" === 0);//false
+
+console.log("" == 1); //false
+
+console.log("" === 1); //false
+
+
+
+
+
+
+
+
+
+
