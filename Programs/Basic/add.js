d831adc516a3d67fda2574ae8af5444e6b45fef7
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-08-31 21:20:46 +0530

    add

---
+++ Programs/Basic/add.js
@@ -0,0 +1,10 @@
+
+const prompt = require("prompt-sync")();
+
+let a=Number(prompt("Enter First number = "))
+
+let b=Number(prompt("Enter Second number ="))
+
+let result=a+b
+
+console.log("Addition is ",result)
