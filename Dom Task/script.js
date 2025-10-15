4c4491870532b011764836f8df5dd3df671b4f9a
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-10-15 21:27:50 +0530

    domtask

---
+++ Dom Task/script.js
@@ -0,0 +1,17 @@
+
+/*
+
+Create a new  Element and Print it after heading  
+*/
+
+
+const newElement=document.createElement("h2")
+
+newElement.textContent="Javascript"
+
+
+const element=document.getElementById("first")
+
+element.after(newElement)
+
+
