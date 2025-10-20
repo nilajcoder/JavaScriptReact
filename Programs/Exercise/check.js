58b3ea2803bd4b5da825c2af1519b9057af7a92a
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-10-20 16:39:06 +0530

    check

---
+++ Programs/Exercise/check.js
@@ -0,0 +1,12 @@
+/*
+Write a JavaScript program to check two numbers 
+and return true if one of the number is 100 or
+ if the sum of the two numbers is 100
+*/
+
+
+const equalto100 =(a,b)=> a===100 || b===100 || (a+b)===100
+
+console.log( equalto100(100,0))
+console.log(equalto100(50,50))
+console.log(equalto100(0,100))
