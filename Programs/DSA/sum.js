d25941ee595b2398208e0e305b18f239c1086c11
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-10-26 18:35:19 +0530

    sumofdigits

---
+++ Programs/DSA/sum.js
@@ -0,0 +1,23 @@
+/*
+
+WAP in Js Input Numbers and add them 
+Sum of digits
+Expample - 531
+  5+3+1=9
+*/
+
+function sumofdigits(num) {
+
+    let sum = 0
+    while (num > 0) {
+
+        sum += num % 10;
+        num = Math.floor(num / 10)
+    }
+
+    return sum;
+
+}
+
+console.log(sumofdigits(53))//8
+console.log(sumofdigits(531))//9
