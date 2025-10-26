55d6ed276c3c83b0b2babb16c12e9f3d364d956b
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-10-26 21:25:09 +0530

    evenoddcheck

---
+++ Programs/DSA/even.js
@@ -0,0 +1,18 @@
+/*
+
+4. Wap in js Check even odd
+
+  Input -4  Even
+         3  odd
+*/
+
+ function evenodd(num){
+
+    if(num%2===0){
+
+        return "Even"
+    } else
+        return "Odd"
+ }
+ console.log(evenodd(5))// Odd
+ console.log(evenodd(6)) // Even
