2d491fd20f77c1de8e5e6bb80f321a7bbdd8ad51
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-11-01 16:06:31 +0530

    swap

---
+++ Programs/DSA/swap.js
@@ -0,0 +1,22 @@
+
+/*
+
+WAP in Js Function SWAP Two Numbers
+
+input -5 6
+
+Output - 6 5
+*/
+
+
+function swap(a, b){
+
+    a=a+b
+    b=a-b
+    a=a-b
+
+    console.log("After Swaping =" ,a,b)
+}
+
+swap(5,6) // 6 5
+swap(10,20)//20 10
