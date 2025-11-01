f7733bb2665b9b6d146486ec0d3e95f735cfc6d8
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-11-01 20:15:18 +0530

    10OnetoN

---
+++ Programs/DSA/oneton.js
@@ -0,0 +1,19 @@
+/*
+
+10. Print 1 to N in Js Function
+
+
+Input-5
+Output -1 2 3 4 5
+*/
+
+
+function onetoN(num){
+
+    for( let i=1;i<=num;i++){
+
+        console.log(i)
+    }
+}
+
+onetoN(5)// 1 2 3 4 5
