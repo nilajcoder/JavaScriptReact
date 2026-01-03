ef0b7bbf1e0284294d84e5d28446af11c1bebb46
nilajcoder <nilaj.chakraborty007@gmail.com>
2026-01-03 19:28:56 +0530

    object

---
+++ Objects/objectexample.js
@@ -0,0 +1,25 @@
+
+
+/**
+ * 
+ * What is Object
+ * 
+ * Ans- Object is a data type in JavaScript that allows you
+ *  to store collections of data as key-value pairs.
+ */
+
+let ball={
+    color:"red",
+    shape:"round",
+    size:"large",
+    name:"soccer ball",
+    price:20
+
+}
+// Accessing object properties
+console.log(ball);
+console.log(ball.color);
+console.log(ball.size);
+console.log(ball.price);
+console.log(ball.name);
+
